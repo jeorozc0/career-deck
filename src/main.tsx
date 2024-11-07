0f6c9ff7f24cc1fa61396456d0a4fe4a03232e57
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-11-07 15:32:58 -0600

    add react query to project

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Dashboard from './pages/dashboard';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
