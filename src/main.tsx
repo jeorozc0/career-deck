c8b365286dcf64f63a98386c50b049f17a1e78c5
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-11-08 13:17:43 -0600

    add toast context to app

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Dashboard from './pages/dashboard';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from './components/ui/toaster';
 
 const queryClient = new QueryClient()
 
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>,
 )
